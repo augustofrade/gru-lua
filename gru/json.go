c680f41c798d0be30ce448c0da4a3326f89fc652
augustofrade <aquaaticnaotrava@gmail.com>
2026-05-28 09:51:31 -0300

    Add json parse function registration

--- gru/json.go
+++ gru/json.go
@@ -13,6 +13,10 @@ func NewJsonModule() GruModule {
 		TableParam("table", "The table to be stringified").
 		ReturnsString().
 		Register()
+	module.FunctionBuilder("parse", "Parses a JSON string into a table.", jsonParse).
+		StringParam("json", "The JSON string to be parsed").
+		Returns("table").
+		Register()
 	return module
 }
 
@@ -23,4 +27,16 @@ func jsonStringify(l *lua.State) int {
 	result := luautil.LuaTableToGo(l, 1)
 	stringfiedValue, _ := json.Marshal(result)
 	return luautil.StringResult(l, string(stringfiedValue))
+}
+
+func jsonParse(l *lua.State) int {
+	if !luautil.IsString(l, 1) {
+		return luautil.ErrorResult(l, "Expected a JSON string")
+	}
+	str, _ := l.ToString(1)
+
+	var result any
+	json.Unmarshal([]byte(str), &result)
+
+	return luautil.StringResult(l, "r")
 }
